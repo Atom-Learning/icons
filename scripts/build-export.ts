8f8fdeece23e6431d82edaea2288b42265872beb
Thomas Digby <tom@atomlearning.co.uk>
2023-01-03 11:16:12 +0000

    fix: remove duplicate import and add display name

--- scripts/build-export.ts
+++ scripts/build-export.ts
@@ -12,8 +12,21 @@ const DIRECTORY_OUTPUT = './dist'
 const optimisedComponentTemplate = (
   { props, imports, componentName, jsx },
   { tpl }
-) =>
-  tpl`${imports};export const ${componentName} = React.forwardRef(${props} => ${jsx})`
+) => {
+  // without this filter the SVGR `transform` adds an additional generic `import "react"`
+  // removing this import still allows SVGR to output `import * as r from "react"` and rewrite
+  // `React.forwardRef` to `r.forwardRef` (r being a random obfuscated character)
+  const filteredImports = imports.filter((importDeclaration) =>
+    importDeclaration?.specifiers.some(
+      (specifier) => specifier?.local?.name !== 'forwardRef'
+    )
+  )
+  return tpl`
+${filteredImports};
+export const ${componentName} = React.forwardRef(${props} => ${jsx});
+${componentName}.displayName = "${componentName}"
+`
+}
 
 const svgoConfig = {
   multipass: true,
